4fadf9f6c821a9cc6a7188732e462cbe0277d5f2
Agis Maulana <agismaulana112@gmail.com>
2020-01-29 11:35:55 +0700

    commit

--- php/logic/logictransaksi2.php
+++ php/logic/logictransaksi2.php
@@ -8,7 +8,7 @@
 // $dataawal = ($jumlahdataperhalaman * $halaktif) - $jumlahdataperhalaman;
 
 $id = $_GET['id'];
-$transaksis = mysqli_query($conn,"SELECT produk.*,kategori.nama as namakategori,transaksi.total as total,                                           transaksi.id as id_transaksi,user.nama as nama_user,                                                       transaksi.jumlah_beli as jumlah,status.nama as status,status.id as                                         id_status 
+$transaksis = mysqli_query($conn,"SELECT produk.*,kategori.nama as namakategori,transaksi.total as total,                                           transaksi.id as id_transaksi,user.nama as nama_user,                                                       transaksi.jumlah_beli as jumlah,status.nama as status,status.id                                            as id_status 
                                  FROM transaksi INNER JOIN produk ON produk.id = transaksi.produk_id INNER      JOIN kategori ON produk.id_kategori = kategori.id INNER JOIN user ON      user.id = transaksi.user_id INNER JOIN status ON status.id =              transaksi.status_id 
                                  WHERE transaksi.id = '$id'");
 $transaksi = mysqli_fetch_assoc($transaksis);
